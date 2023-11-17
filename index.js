d5450e2b5c00d32c536cf5bbfd50924d8ee8cd4c
theborgh <borghiborghi@gmail.com>
2023-11-17 10:37:45 +0100

    update stats after reading

--- index.js
+++ index.js
@@ -4,9 +4,46 @@ const util = require("util");
 
 const client = new textToSpeech.TextToSpeechClient();
 
+const readStats = () => {
+  const statsText = fs.readFileSync("stats.csv", "utf8");
+  const entries = statsText.split("\r\n");
+  const stats = {};
+  const today = new Date();
+
+  for (const entry of entries) {
+    const [voiceType, period, charCount] = entry.split(",");
+    stats[voiceType] = { period, charCount: parseInt(charCount) };
+  }
+
+  // update stats if needed
+  for (const entry in stats) {
+    if (
+      entry.toString().split(" ")[1] === "d" &&
+      stats[entry].period !== today.toISOString().slice(0, 10)
+    ) {
+      stats[entry].period = today.toISOString().slice(0, 10);
+      stats[[entry].toString().split(" ")[0] + " m"].charCount +=
+        stats[entry].charCount;
+      stats[entry].charCount = 0;
+    }
+
+    if (
+      entry.toString().split(" ")[1] === "m" &&
+      stats[entry].period !== today.toISOString().slice(0, 7)
+    ) {
+      stats[entry].period = today.toISOString().slice(0, 7);
+      stats[entry].charCount = 0;
+    }
+  }
+
+  return stats;
+};
+
 async function readTranscript() {
   const inputText = fs.readFileSync("input.txt", "utf8");
-  const stats = fs.readFileSync("input.txt", "utf8");
+  const stats = readStats();
+
+  console.log(stats);
 
   const freeTiers = [
     { voiceType: "Neural2", freeCharsPerMonth: 800_000 }, // 1_000_000 bytes
@@ -47,44 +84,44 @@ async function readTranscript() {
   const audioContent = [];
   const startTime = new Date();
 
-  for (const chunk of chunks) {
-    // Get the voice code for the chunk
-    let voiceCode = defaultVoice;
-    let voiceSeparator = null;
+  // for (const chunk of chunks) {
+  //   // Get the voice code for the chunk
+  //   let voiceCode = defaultVoice;
+  //   let voiceSeparator = null;
 
-    for (const characterVoice of characterVoices) {
-      if (chunk.match(characterVoice.separator)) {
-        voiceCode = characterVoice.voiceCode;
-        voiceSeparator = characterVoice.separator;
-        break;
-      }
-    }
+  //   for (const characterVoice of characterVoices) {
+  //     if (chunk.match(characterVoice.separator)) {
+  //       voiceCode = characterVoice.voiceCode;
+  //       voiceSeparator = characterVoice.separator;
+  //       break;
+  //     }
+  //   }
 
-    const request = {
-      input: { text: chunk.replace(voiceSeparator, "") },
-      voice: {
-        languageCode: voiceCode.split("-")[0] + "-" + voiceCode.split("-")[1],
-        name: voiceCode,
-      },
-      audioConfig: { audioEncoding: "MP3" },
-    };
+  //   const request = {
+  //     input: { text: chunk.replace(voiceSeparator, "") },
+  //     voice: {
+  //       languageCode: voiceCode.split("-")[0] + "-" + voiceCode.split("-")[1],
+  //       name: voiceCode,
+  //     },
+  //     audioConfig: { audioEncoding: "MP3" },
+  //   };
 
-    const [response] = await client.synthesizeSpeech(request);
-    audioContent.push(response.audioContent);
-  }
+  //   const [response] = await client.synthesizeSpeech(request);
+  //   audioContent.push(response.audioContent);
+  // }
 
-  const concatenatedAudio = Buffer.concat(audioContent);
+  // const concatenatedAudio = Buffer.concat(audioContent);
 
-  // Write the binary audio content to a local file
-  const writeFile = util.promisify(fs.writeFile);
-  await writeFile("output.mp3", concatenatedAudio, "binary");
-  const endTime = new Date();
+  // // Write the binary audio content to a local file
+  // const writeFile = util.promisify(fs.writeFile);
+  // await writeFile("output.mp3", concatenatedAudio, "binary");
+  // const endTime = new Date();
 
-  console.log(
-    `Audio content generated in ${
-      (endTime - startTime) / 1000
-    } seconds and written to output.mp3`
-  );
+  // console.log(
+  //   `Audio content generated in ${
+  //     (endTime - startTime) / 1000
+  //   } seconds and written to output.mp3`
+  // );
 }
 
 readTranscript();
